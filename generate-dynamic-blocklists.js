f40773e43d057d1c961110d657fb9b28e5740226
ExponentialWorkload <90570076+Exponential-Workload@users.noreply.github.com>
2023-05-31 14:07:59 +0200

    fix: Fix v6 nl

--- generate-dynamic-blocklists.js
+++ generate-dynamic-blocklists.js
@@ -9,6 +9,6 @@ const fs = require('fs');
   const binaryEdgeIPs = binaryEdgeJson.scanners;
   fs.writeFileSync(__dirname+'/lists/binary-edge.sh',fs.readFileSync(__dirname+'/lists/binary-edge.sh','utf-8')+
 `iptablesIO -s ${binaryEdgeIPs.filter(v=>v.includes('.')).join(',')} -j DROP
-${binaryEdgeIPs.filter(v=>v.includes(':')).length > 0 ? `
-iptablesIO -s ${binaryEdgeIPs.filter(v=>v.includes(':')).join(',')} -j DROP` : ''}`)
+${binaryEdgeIPs.filter(v=>v.includes(':')).length > 0 ? `iptablesIO -s ${binaryEdgeIPs.filter(v=>v.includes(':')).join(',')} -j DROP
+` : ''}`)
 })();
